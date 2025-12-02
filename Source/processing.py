1f0a2a805e4e429583789ed68c82b342de8fab25
Shegun93 <shegunholadele@gmail.com>
2025-12-02 15:40:23 +0000

    updated precoess

--- Source/processing.py
+++ Source/processing.py
@@ -9,22 +9,16 @@ def load_data():
     y = data.target
     return X, y
 
-
 def split_data(X, y, test_size=0.2, seed=42):
-    return train_test_split(
-        X,
-        y,
-        test_size=test_size,
-        random_state=seed,
-        stratify=y
+    return train_test_split(X, 
+                            y,
+                            test_size=test_size,
+                            random_state=seed,
+                            stratify=y
     )
-
 
 def preprocess(X_train, X_test):
     scaler = StandardScaler()
-
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
-
-    # return scaler as third element
     return X_train_scaled, X_test_scaled, scaler
